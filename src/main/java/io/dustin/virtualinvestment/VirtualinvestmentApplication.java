9c409c1677b65231bbd7962e962539e7bcce9013
GiWoonHwang <85157729+GiWoonHwang@users.noreply.github.com>
2025-06-14 09:35:35 +0900

    Init

---
+++ src/main/java/io/dustin/virtualinvestment/VirtualinvestmentApplication.java
@@ -0,0 +1,13 @@
+package io.dustin.virtualinvestment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VirtualinvestmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VirtualinvestmentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/dustin/virtualinvestment/VirtualinvestmentApplicationTests.java
@@ -0,0 +1,13 @@
+package io.dustin.virtualinvestment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VirtualinvestmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
